bc6515063b4162705698e4eed990f613edefdd04
briansune <briansune@gmail.com>
2022-06-08 22:22:40 +0800

    up

---
+++ sw/sampleapp.py
@@ -0,0 +1,18 @@
+import array
+import riffa
+
+amt = 200
+sent = 0
+recv = 0
+dataSend = array.array('I', range(amt))
+dataSend = dataSend[0:]
+dataRecv = array.array('I', [0] * amt)
+
+fd = riffa.fpga_open(0)
+sent = riffa.fpga_send(fd, 0, dataSend, amt, 0, True, 0)
+if sent != 0:
+    recv = riffa.fpga_recv(fd, 0, dataRecv, 0)
+
+riffa.fpga_close(fd)
+print(dataSend)
+print(dataRecv)
